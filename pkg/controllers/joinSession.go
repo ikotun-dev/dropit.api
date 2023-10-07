6c87a4e706d06dba346fc7b77bfdfe9b2ad7c5ab
ikotun-dev <danlogan2003@gmail.com>
2023-10-07 16:19:28 +0100

    joinSession config

--- pkg/controllers/joinSession.go
+++ pkg/controllers/joinSession.go
@@ -1,1 +1,25 @@
 package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// function to join session
+func JoinSession(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	vars := mux.Vars(r)
+	session_key := vars["session_key"]
+	if len(session_key) < 7 {
+		res := map[string]string{"error": "session key should not be less than 7 characters"}
+		errorMessage, err := json.Marshal(res)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		w.Write([]byte(errorMessage))
+
+	}
+
+}
