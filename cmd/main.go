7c48bd2ebef3d8b699cd0dfa4ab3bfad85c15aa5
ikotun-dev <danlogan2003@gmail.com>
2023-10-07 15:56:57 +0100

    main router setup

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/ikotun-dev/clipsync/pkg/router"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
 func main() {
+	r := mux.NewRouter()
+	router.RoutingRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:8000", r))
 
 }
