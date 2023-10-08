d637be4073df288f47a7ef0180ab614e685a0e9c
ikotun-dev <danlogan2003@gmail.com>
2023-10-08 10:56:57 +0100

    Minor refactor to validate token

--- pkg/middleware/validateJwt.go
+++ pkg/middleware/validateJwt.go
@@ -2,10 +2,12 @@ package middleware
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/dgrijalva/jwt-go"
 )
 
+/*
 func ValidateJWT(next func(w http.ResponseWriter, r *http.Request)) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Header["Token"] != nil {
@@ -29,5 +31,47 @@ func ValidateJWT(next func(w http.ResponseWriter, r *http.Request)) http.Handler
 			w.WriteHeader(http.StatusUnauthorized)
 			w.Write([]byte("unauthorized"))
 		}
+	})
+}
+*/
+
+func ValidateJWT(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Check for the "Authorization" header
+		authHeader := r.Header.Get("Authorization")
+		if authHeader != "" {
+			// Split the header value into "Bearer" and the token
+			parts := strings.Split(authHeader, " ")
+			if len(parts) != 2 || parts[0] != "Bearer" {
+				w.WriteHeader(http.StatusUnauthorized)
+				w.Write([]byte("Unauthorized: Invalid token format"))
+				return
+			}
+
+			tokenString := parts[1]
+
+			token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+				_, ok := t.Method.(*jwt.SigningMethodHMAC)
+				if !ok {
+					w.WriteHeader(http.StatusUnauthorized)
+					w.Write([]byte("Unauthorized: Invalid token signing method"))
+				}
+				return SECRET, nil
+			})
+
+			if err != nil {
+				w.WriteHeader(http.StatusUnauthorized)
+				w.Write([]byte("Unauthorized: " + err.Error()))
+				return
+			}
+
+			if token.Valid {
+				next.ServeHTTP(w, r)
+				return
+			}
+		}
+
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte("Unauthorized: Missing or invalid token"))
 	})
 }
