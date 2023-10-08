909a532851bc6eaeeef1e636c0411b1ac8ee2243
ikotun-dev <danlogan2003@gmail.com>
2023-10-08 11:26:19 +0100

    websocket setup

---
+++ pkg/socket/handler.go
@@ -0,0 +1,16 @@
+package socket
+
+import (
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func WebSocketHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+}
