4e72492304b244dfd01bd93ff425fffee361b8a1
ikotun-dev <danlogan2003@gmail.com>
2023-10-19 21:43:56 +0100

    Removed

--- pkg/router/routes.go
+++ pkg/router/routes.go
@@ -1,17 +1,10 @@
 package router
 
 import (
-	"net/http"
 	"github.com/gorilla/mux"
-	"github.com/gorilla/handlers"
 	"github.com/ikotun-dev/clipsync/pkg/controllers"
 )
 
-allowedOrigins := handlers.AllowedOrigins([]string{"*"})
-allowedHeaders := handlers.AllowedHeaders([]string{"Content-Type"})
-allowedMethods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE"})
-
-corsHandler := handlers.CORS(allowedOrigins, allowedHeaders, allowedMethods)(r)
 var RoutingRoutes = func(router *mux.Router) {
 	router.HandleFunc("/session/new", controllers.CreateSession).Methods("POST")
 	router.HandleFunc("/session/ws", controllers.JoinSession).Methods("POST")
